3df73b3e7947c6fa247a914347d1c70ba5341804
MD.Maruf Siddiki <marufsiddiki63@gmail.com>
2026-03-31 23:16:39 +0600

    Add files via upload

---
+++ running_time_of_algorithms.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int runningTime(vector<int> arr) {
+    int n = arr.size();
+    int shifts = 0;
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+            shifts++;
+        }
+        arr[j + 1] = key;
+    }
+
+    return shifts;
+}
+
+int main() {
+    int n;
+    if (!(cin >> n)) return 0;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << runningTime(arr) << endl;
+
+    return 0;
+}
